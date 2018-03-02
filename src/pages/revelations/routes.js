46dfb28a15f681676289a844d93c4ac8b7e6ad5d
Sam Grundman <samuel@yodas.ws>
2018-03-02 10:49:26 -0500

    Correct template path

--- src/pages/revelations/routes.js
+++ src/pages/revelations/routes.js
@@ -3,7 +3,7 @@
 angular.module('pageRevelations')
 .config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/revelations/', {
-		templateUrl: 'pages/creation/revelations.html',
+		templateUrl: 'pages/revelations/revelations.html',
 		controllerAs: '$ctrl',
 		controller() {
 			angular.element('[ng-view]').attr('ng-view', 'pageRevelations')
