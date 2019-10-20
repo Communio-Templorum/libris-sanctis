79d53071aaf1d8a5834822b13ddc17264ec9c506
Sam Grundman <samuel@yodas.ws>
2019-10-19 22:05:43 -0400

    Add Table of Contents for The Odyssey

--- src/pages/odyssey/book1/ctrl.js
+++ src/pages/odyssey/book1/ctrl.js
@@ -1,6 +1,4 @@
 yodasws.page('odyssey/book1').setRoute({
 	template: 'pages/odyssey/book1/book1.html',
 	route: '/odyssey/book1/',
-}).on('load', () => {
-	console.log('Page loaded!');
 });
---
+++ src/pages/odyssey/ctrl.js
@@ -0,0 +1,4 @@
+yodasws.page('odyssey').setRoute({
+	template: 'pages/odyssey/odyssey.html',
+	route: '/odyssey/',
+});
