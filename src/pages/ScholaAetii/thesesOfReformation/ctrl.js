10a1177ca3ed0b6c4bb721a03f09f2afb4aeeefa
Sam Grundman <samuel@yodas.ws>
2020-09-07 17:33:23 -0400

    Add new books of Modern Interpretation

---
+++ src/pages/ScholaAetii/thesesOfReformation/ctrl.js
@@ -0,0 +1,6 @@
+yodasws.page('pageThesesOfReformation').setRoute({
+	template: 'pages/ScholaAetii/thesesOfReformation/thesesOfReformation.html',
+	route: '/ScholaAetii/thesesOfReformation/',
+}).on('load', () => {
+	console.log('Page loaded!');
+});
