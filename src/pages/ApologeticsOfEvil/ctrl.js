a8a42e907aa5c8b8c2a03e1ec1adaaf86c24b385
Sam Grundman <samuel@yodas.ws>
2020-09-07 17:34:20 -0400

    Add new book of Apologetics

---
+++ src/pages/ApologeticsOfEvil/ctrl.js
@@ -0,0 +1,7 @@
+yodasws.page('ApologeticsOfEvil').setRoute({
+	template: 'pages/ApologeticsOfEvil/ApologeticsOfEvil.html',
+	canonicalRoute: '/ApologeticsOfEvil/',
+	route: '/ApologeticsOfEvil/?',
+}).on('load', () => {
+	console.log('Page loaded!');
+});
