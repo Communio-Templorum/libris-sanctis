c28804b01a231829dfbcd27affd70397efcaf426
Sam Grundman <samuel@yodas.ws>
2020-10-13 13:59:05 -0400

    Add Nostoi and Telegony books, as best we have

---
+++ src/pages/nostoi/ctrl.js
@@ -0,0 +1,5 @@
+yodasws.page('nostoi').setRoute({
+	template: 'pages/nostoi/nostoi.html',
+	canonicalRoute: '/nostoi/',
+	route: '/nostoi/?',
+});
---
+++ src/pages/telegony/ctrl.js
@@ -0,0 +1,5 @@
+yodasws.page('telegony').setRoute({
+	template: 'pages/telegony/telegony.html',
+	canonicalRoute: '/telegony/',
+	route: '/telegony/?',
+});
