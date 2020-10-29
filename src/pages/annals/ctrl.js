29d821c438652035fcad5c16cecb885fad20acdf
Sam Grundman <samuel@yodas.ws>
2020-10-28 20:57:46 -0400

    Start annals listing kings/rulers for every year (like the Fasti Capitolini)

---
+++ src/pages/annals/ctrl.js
@@ -0,0 +1,7 @@
+yodasws.page('annals').setRoute({
+	template: 'pages/annals/annals.html',
+	canonicalRoute: '/annals/',
+	route: '/annals/?',
+}).on('load', () => {
+	console.log('Page loaded!');
+});
