8b8e5f76e533a932dc380d64da4c5b8f1c26f0ab
Sam Grundman <samuel@yodas.ws>
2021-04-01 22:14:41 -0400

    Add first three Homeric Hymns and link books together

---
+++ src/pages/homericHymns/ctrl.js
@@ -0,0 +1,7 @@
+yodasws.page('pageHomericHymns').setRoute({
+	title: 'Homeric Hymns',
+	template: 'pages/homericHymns/homericHymns.html',
+	canonicalRoute: '/homericHymns/',
+	route: '/homeric-?[Hh]ymns/?',
+}).on('load', () => {
+});
