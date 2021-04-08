844159b4ded5af88eab0271120b459b7fbb5cac3
Sam Grundman <samuel@yodas.ws>
2021-04-08 13:03:03 -0400

    Add unedited/unredacted copy of "On the True Doctrine"
    and update Cyperia and hymns pages with a11y improvements and refactoring

---
+++ src/pages/trueDoctrine/ctrl.js
@@ -0,0 +1,6 @@
+yodasws.page('pageTrueDoctrine').setRoute({
+	title: 'On the True Doctrine',
+	template: 'pages/trueDoctrine/trueDoctrine.html',
+	canonicalRoute: '/trueDoctrine/',
+	route: '/true-?[Dd]octrine/?',
+});
