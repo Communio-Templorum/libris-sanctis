83302acfabfeee19af7f9f6bb414d7bb002fbf44
Sam Grundman <samuel@yodas.ws>
2021-09-08 09:42:02 -0400

    Add checkbox to collapse/expand all sections

--- src/pages/creation/module.js
+++ src/pages/creation/module.js
@@ -3,4 +3,14 @@ yodasws.page('pageCreation').setRoute({
 	template: 'pages/creation/creation.html',
 	canonicalRoute: '/creation/',
 	route: '/creation/?',
+}).on('load.page', () => {
+	const name = document.querySelector('[name="collapse"]');
+	if (name instanceof Element) {
+		name.addEventListener('input', (evt) => {
+			[...document.querySelectorAll('details')].forEach((el) => {
+				if (name.checked) el.setAttribute('open', 'open');
+				else el.removeAttribute('open');
+			});
+		});
+	}
 });
