c1e4c07007124c79c1e1d94bca4e9c917c15cb39
Sam Grundman <samuel@yodas.ws>
2021-10-03 13:49:15 -0400

    Add Book 1 of Metamorphoses

---
+++ src/pages/metamorphoses/Book1/ctrl.js
@@ -0,0 +1,6 @@
+yodasws.page('pageMetamorphosesBook1').setRoute({
+	title: 'Metamorphoses, Book 1',
+	template: 'pages/metamorphoses/Book1/Book1.html',
+	canonicalRoute: '/metamorphoses/book1/',
+	route: '/metamorphoses/book1/?',
+});
