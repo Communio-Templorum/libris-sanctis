f62792b18e0a92a340d10a4836637df0a5203752
Sam Grundman <samuel@yodas.ws>
2021-11-07 11:28:42 -0500

    Add Roman Questions of Plutarch, 1-9

---
+++ src/pages/RomanQuestions/ctrl.js
@@ -0,0 +1,7 @@
+yodasws.page('pageRomanQuestions').setRoute({
+	title: 'Roman Questions',
+	template: 'pages/RomanQuestions/RomanQuestions.html',
+	canonicalRoute: '/RomanQuestions/',
+	route: '/RomanQuestions/?',
+}).on('load', () => {
+});
